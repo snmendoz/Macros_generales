5fc977cbf656dc23fa45039ba803bc3addd5d631
Sebastián Mendoza Farías <snmendoz@uc.cl>
2024-04-18 09:48:47 -0400

    update createRulesFromgrRules

--- createRulesFromgrRules.m
+++ createRulesFromgrRules.m
@@ -23,54 +23,78 @@ end
 % involvedGenes = cellfun(@unique,cellfun(@splitString,regexprep(grRules,'\or|and|\(|\)|&|\|',' '),'UniformOutput',0),'UniformOutput',0);
 pos = find(~cellfun(@isempty, involvedGenes));
 for i = 1:length(pos)
-%     if pos(i) ==410
-%        disp('') 
-%     end
+    if pos(i) ==272
+       disp('') 
+    end
     genes_i = unique(involvedGenes{pos(i)});
 %     if pos(i)==getPosOfElementsInArray({'TRE6PP'},model.rxns)
 %         disp('')
 %     end
     for j = 1:length(genes_i)
-        condition1 = ['^' genes_i{j} '$' ... %YALI0F04$
-        '|^(' genes_i{j} ')$'...%^(YALI0F04)$    
-        '|^' genes_i{j} '\ '... %^YALI0F04\E 
-        '|^(' genes_i{j} '\ '....%^(YALI0F04\E 
-        '|^(' genes_i{j} ')\ ' ... %^(YALI0F04)E
-        '|\ ' genes_i{j} '$'... %\ YALI0F04$
-        '|\ (' genes_i{j} ')$'...  %(YALI0F04)$
-        '|\ ' genes_i{j} ')$'...  %YALI0F04)$
-        '|\ ' genes_i{j} '\ '... %\ YALI0F04\E 
-        '|^\ (' genes_i{j} '\ '....%\E(YALI0F04\E
-        '|\ ' genes_i{j} ')\ '... %\EYALI0F04)\E 
-        '|\ (' genes_i{j} ')\ ']; %\E(YALI0F04)\E 
-    
+%         condition1 = ['^' genes_i{j} '$' ... %YALI0F04$
+%         '|^(' genes_i{j} ')$'...%^(YALI0F04)$    
+%         '|^' genes_i{j} '\ '... %^YALI0F04\E 
+%         '|^(' genes_i{j} '\ '....%^(YALI0F04\E 
+%         '|^(' genes_i{j} ')\ ' ... %^(YALI0F04)E
+%         '|\ ' genes_i{j} '$'... %\ YALI0F04$
+%         '|\ (' genes_i{j} ')$'...  %(YALI0F04)$
+%         '|\ ' genes_i{j} ')$'...  %YALI0F04)$
+%         '|\ ' genes_i{j} '\ '... %\ YALI0F04\E 
+%         '|^\ (' genes_i{j} '\ '....%\E(YALI0F04\E
+%         '|\ ' genes_i{j} ')\ '... %\EYALI0F04)\E 
+%         '|\ (' genes_i{j} ')\ ']; %\E(YALI0F04)\E 
+        
+        %posibilities
+%^GEN$
+%^GEN\E 
+%^GEN\)
+%\(GEN$
+%\(GEN\E
+%\(GEN\)
+%\ GEN$
+%\ GEN\E
+%\ GEN\)
+
         conditions = {['^' genes_i{j} '$'] ... %YALI0F04$
-        ['^\(' genes_i{j} '\)$']...%^(YALI0F04)$    
-        ['^' genes_i{j} '(?=\ )']... %^YALI0F04\E 
-        ['^\(' genes_i{j} '(?=\ )']....%^(YALI0F04\E 
-        ['^\(' genes_i{j} '\)(?=\ )'] ... %^(YALI0F04)E
-        ['(?<=\ )' genes_i{j} '$']... %\ YALI0F04$
-        ['(?<=\ )\(' genes_i{j} '\)$']...  %(YALI0F04)$
-        ['(?<=\ )' genes_i{j} '\)$']...  %YALI0F04)$
-        ['(?<=\ )' genes_i{j} '(?=\ )']... %\ YALI0F04\E 
-        ['(?<=\ )\(' genes_i{j} '(?=\ )']....%\E(YALI0F04\E
-        ['(?<=\ )' genes_i{j} '\)(?=\ )']... %\EYALI0F04)\E 
-        ['(?<=\ )\(' genes_i{j} '\)(?=\ )']...
-        ['(?<=\()\(' genes_i{j} '\)(?=\ )']...
-        ['(?<=\ )\(' genes_i{j} '\)(?=\))']...
-        ['(?<=\()\(' genes_i{j} '\)(?=\))']...
-        ['^\(\(' genes_i{j} '(?=\ )'],...
-        ['^\(\(\(' genes_i{j} '(?=\ )'],...
-        ['(?<=\ )' genes_i{j} '\)\)$'],...
-        ['(?<=\ )' genes_i{j} '\)\)\)$'],...
-        ['\(\(' genes_i{j} '(?=\ )'],...
-        ['(?<=\ )' genes_i{j} '\)\)']}; 
+            ['^' genes_i{j} '(?=\ )'] ... %YALI0F04$
+            ['(?<=\()' genes_i{j} '(?=\))']...
+        ['(?<=\()' genes_i{j} '(?=\ )']...
+        ['(?<=\ )' genes_i{j} '(?=\))']...
+        ['(?<=\ )' genes_i{j} '$']...
+        ['(?<=\ )' genes_i{j} '(?=\ )']
+        };
+
+        %conditions
+%         conditions = {['^' genes_i{j} '$'] ... %YALI0F04$
+%         ['^\(' genes_i{j} '\)$']...%^(YALI0F04)$    
+%         ['^' genes_i{j} '(?=\ )']... %^YALI0F04\E 
+%         ['^\(' genes_i{j} '(?=\ )']....%^(YALI0F04\E 
+%         ['^\(' genes_i{j} '\)(?=\ )'] ... %^(YALI0F04)E
+%         ['(?<=\ )' genes_i{j} '$']... %\ YALI0F04$
+%         ['(?<=\ )' genes_i{j} '\)$']...  %(YALI0F04)$
+%         ['(?<=\ )' genes_i{j} '\)$']...  %YALI0F04)$
+%         ['(?<=\ )' genes_i{j} '(?=\ )']... %\ YALI0F04\E 
+%         ['(?<=\ )\(' genes_i{j} '(?=\ )']....%\E(YALI0F04\E
+%         ['(?<=\ )' genes_i{j} '\)(?=\ )']... %\EYALI0F04)\E 
+%         ['(?<=\ )\(' genes_i{j} '\)(?=\ )']...
+%         ['(?<=\()\(' genes_i{j} '\)(?=\ )']...
+%         ['(?<=\ )\(' genes_i{j} '\)(?=\))']...
+%         ['(?<=\()\(' genes_i{j} '\)(?=\))']};%...
+%         ['^\(\(' genes_i{j} '(?=\ )'],...
+%         ['^\(\(\(' genes_i{j} '(?=\ )'],...
+%         ['(?<=\ )' genes_i{j} '\)\)$'],...
+%         ['(?<=\ )' genes_i{j} '\)\)\)$'],...
+%         ['\(\(' genes_i{j} '(?=\ )'],...
+%         ['(?<=\ )' genes_i{j} '\)\)']}; 
 %       
         for k =1:length(conditions)
-%             if pos(i) ==410
+%             
 %                 regexprep(rules{pos(i)}, conditions{k} , ['x(' num2str(find(strcmp(genes_i{j}, model.genes))) ')'])
 %             end
             rules{pos(i)} = regexprep(rules{pos(i)}, conditions{k} , ['x(' num2str(find(strcmp(genes_i{j}, model.genes))) ')']);
+%             if pos(i) ==272
+%                 disp(rules{pos(i)});
+%             end
         end
 %         condition = ['^' genes_i{j} '$|^' genes_i{j} '(?=\ )|(?<=\ )' genes_i{j} '$|(?<=\ )'  genes_i{j} '(?=\ )|(' genes_i{j} '(?=\ )|(?<=\ )' genes_i{j} ')|(' genes_i{j} ')'];
 %         rules{pos(i)} = regexprep(rules{pos(i)}, condition , ['x(' num2str(find(strcmp(genes_i{j}, model.genes))) ')']);
